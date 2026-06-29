8d4c3b14a55a0f0fca7fc47bf51725f37eab9a6f
Vishwas Middha <dkmidha70@gmail.com>
2026-06-29 08:00:28 +0530

    Add Creating Keys for StORages Has Become My Main Skill [C] from Codeforces

---
+++ 2072/C - Creating Keys for StORages Has Become My Main Skill/solution.cpp
@@ -0,0 +1,184 @@
+// Problem: C. Creating Keys for StORages Has Become My Main Skill
+// Contest: Codeforces - Codeforces Round 1006 (Div. 3)
+// URL: https://codeforces.com/contest/2072/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+ 
+// Problem: B. Pashmak and Flowers
+// Contest: Codeforces - Codeforces Round 261 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/459/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <vector>
+#include <stack>
+using namespace std;
+const long long INF = 9223372036854775807;
+const long long MOD = 1e9 + 7;
+ 
+ 
+template<typename T>
+void readvector(vector<T>& v, int n) {
+    v.resize(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+}
+ 
+#define REP(n) for(int t=0; t<n; t++)
+#define FOR(i, n) for (i = 0; i < n; i++)
+#define FFOR(i, j, n) for(i = j; j < n; j++)
+#define FOR_S(i, n, k) for (i = 0; i < n; i += k)
+#define RFOR(i, n) for (i = n - 1; i >= 0; i--)
+#define RFOR_S(i, n, k) for (i = n - 1; i >= 0; i -= k)
+#define MAX_OF(x, y) ((x > y) ? x : y)
+#define MIN_OF(x, y) ((x > y) ? y : x)
+#define MIN_IN(A) *(std::min_element(A.begin(), A.end()))
+#define MAX_IN(A) *(std::max_element(A.begin(), A.end()))
+#define MAX_AT(A) (std::max_element(A.begin(), A.end()) - A.begin())
+#define MIN_AT(A) (std::min_element(A.begin(), A.end()) - A.begin())
+#define COUNT(V,A) count(V.begin(), V.end(), A);
+#define TESTCASE                                                              \
+int testcase;                                                               \
+std::cin>>testcase;                                                         \
+while(testcase--)
+#define GCD(a,b) std::__gcd(a,b);
+#define LCM(a, b) ((a) / std::__gcd((a), (b)) * (b))
+#define SORT(A) std::sort(A.begin(), A.end());
+#define RSORT(A) std::sort(A.rbegin(), A.rend());
+#define REVERSE(A) std::reverse(A.begin(), A.end());
+#define ANY(container, result, condition)                                      \
+result = false;                                                              \
+for (const auto &e : container)                                              \
+    if (condition(e)) {                                                        \
+    result = true;                                                           \
+    break;                                                                   \
+    }
+#define ALL(container, result, condition)                                      \
+result = true;                                                               \
+for (const auto &e : container)                                              \
+    if (!condition(e)) {                                                       \
+    result = false;                                                          \
+    break;                                                                   \
+    }
+template<typename... Args>
+void print(Args... args) {
+    ((cout << args << " "), ...);
+    cout << "
+";
+}
+ 
+#define ll long long
+#define LL long long
+#define pb push_back
+#define pop pop_back
+ 
+LL power(LL x, unsigned long long y) {
+    // returns  x^y % p
+    LL res = 1;
+    x = x % MOD; 
+    while (y > 0) {  
+        if (y & 1) 
+            res = (res*x) % MOD;  
+        y = y>>1;
+        x = (x*x) % MOD;   
+    } 
+    return res; 
+ }
+ 
+ 
+ 
+void speed(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+int main(){
+    speed();
+    TESTCASE{
+    	ll n;
+    	ll x;
+    	cin >> n >> x;
+    	int bits=0;
+    	ll i=1;
+    	while(i&x){
+    		bits++;
+    		i = i<<1;
+    	}
+    	i=1;
+    	int highestbit=-1;
+    	while(i<=x){
+    		highestbit++;
+    		i=i<<1;
+    	}
+    	ll highnum = 1<<highestbit;
+    	ll num = 1<<bits;
+    	if(num<n){
+    		for(ll i=0;i<num;i++){
+    			cout << i << " ";
+    		}
+    		for(ll i=0;i<n-num;i++){
+    			cout << x << " ";
+    		}
+    		cout << "
+";
+    	} else {
+    		for(ll i =0;i<n-1;i++){
+    			cout << i << " ";
+    		}
+    		cout << (n-1>=highnum?n-1:x) << "
+";
+    	}
+    	//cout << highnum << "
+";
+    	
+    	
+    	
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
