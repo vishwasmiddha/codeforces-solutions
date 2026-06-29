9b6a97e80021946006bfb33779d18cac33f8c529
Vishwas Middha <dkmidha70@gmail.com>
2026-06-29 08:02:43 +0530

    Add Goblin [F] from Codeforces

---
+++ 2106/F - Goblin/solution.cpp
@@ -0,0 +1,181 @@
+// Problem: F. Goblin
+// Contest: Codeforces - Codeforces Round 1020 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/2106/F
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+ 
+// Problem: B. Pashmak and Flowers
+// Contest: Codeforces - Codeforces Round 261 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/459/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <stack>
+using namespace std;
+const long long INF = 9223372036854775807;
+const long long MOD = 1e9 + 7;
+ 
+ 
+template<typename T>
+void readvector(vector<T>& v, int n) {
+    v.resize(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+}
+ 
+template <typename T>
+auto SUM(const T& A) {
+    using Elem = typename T::value_type;
+    return std::accumulate(A.begin(), A.end(), Elem(0));
+}
+ 
+#define REP(n) for(int t=0; t<n; t++)
+#define FOR(i, n) for (i = 0; i < n; i++)
+#define FFOR(i, j, n) for(i = j; j < n; j++)
+#define FOR_S(i, n, k) for (i = 0; i < n; i += k)
+#define RFOR(i, n) for (i = n - 1; i >= 0; i--)
+#define RFOR_S(i, n, k) for (i = n - 1; i >= 0; i -= k)
+#define MAX_OF(x, y) ((x > y) ? x : y)
+#define MIN_OF(x, y) ((x > y) ? y : x)
+#define MIN_IN(A) *(std::min_element(A.begin(), A.end()))
+#define MAX_IN(A) *(std::max_element(A.begin(), A.end()))
+#define MAX_AT(A) (std::max_element(A.begin(), A.end()) - A.begin())
+#define MIN_AT(A) (std::min_element(A.begin(), A.end()) - A.begin())
+#define COUNT(V,A) count(V.begin(), V.end(), A);
+#define TESTCASE                                                              \
+int testcase;                                                               \
+std::cin>>testcase;                                                         \
+while(testcase--)
+#define GCD(a,b) std::__gcd(a,b);
+#define LCM(a, b) ((a) / std::__gcd((a), (b)) * (b))
+#define SORT(A) std::sort(A.begin(), A.end());
+#define RSORT(A) std::sort(A.rbegin(), A.rend());
+#define REVERSE(A) std::reverse(A.begin(), A.end());
+#define ANY(container, result, condition)                                      \
+result = false;                                                              \
+for (const auto &e : container)                                              \
+    if (condition(e)) {                                                        \
+    result = true;                                                           \
+    break;                                                                   \
+    }
+#define ALL(container, result, condition)                                      \
+result = true;                                                               \
+for (const auto &e : container)                                              \
+    if (!condition(e)) {                                                       \
+    result = false;                                                          \
+    break;                                                                   \
+    }
+template<typename... Args>
+void print(Args... args) {
+    ((cout << args << " "), ...);
+    cout << "
+";
+}
+ 
+#define ll long long
+#define LL long long
+#define vec vector
+#define pb push_back
+#define popb pop_back
+ 
+LL power(LL x, unsigned long long y) {
+    // returns  x^y % p
+    LL res = 1;
+    x = x % MOD; 
+    while (y > 0) {  
+        if (y & 1) 
+            res = (res*x) % MOD;  
+        y = y>>1;
+        x = (x*x) % MOD;   
+    } 
+    return res; 
+ }
+ 
+ 
+ 
+void speed(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+void recurse(string &s, ll &topsize, ll &downsize, ll &maxsize, int &ind, int n){
+	if(ind==n){
+		return;
+	}
+	if(s[ind]=='1'){
+		topsize=downsize+1;
+		downsize=0;
+		maxsize=max(topsize,max(downsize,maxsize));
+	} else {
+		topsize+=ind;
+		downsize+=n-ind-1;
+		maxsize=max(topsize,max(downsize,maxsize));
+	}
+	//print(ind,topsize,downsize);
+	ind++;
+	recurse(s,topsize,downsize,maxsize,ind,n);
+}
+ 
+int main(){
+    speed();
+	TESTCASE{
+		int n;
+		cin >> n;
+		string s;
+		cin >> s;
+		ll upsize=0, downsize=0, maxsize=0;
+		int ind = 0;
+		recurse(s,upsize,downsize,maxsize,ind,n);
+		cout << maxsize << "
+";
+	}
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
