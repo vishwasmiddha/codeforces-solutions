90ae784d75db84e7e250a7c5028eab01454ba97f
Vishwas Middha <dkmidha70@gmail.com>
2026-06-29 08:02:32 +0530

    Add Subset Multiplication [C] from Codeforces

---
+++ 2124/C - Subset Multiplication/solution.cpp
@@ -0,0 +1,185 @@
+// Problem: C. Subset Multiplication
+// Contest: Codeforces - EPIC Institute of Technology Round Summer 2025 (Codeforces Round 1036, Div. 1 + Div. 2)
+// URL: https://codeforces.com/contest/2124/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 3000 ms
+ 
+// Problem: B. Pashmak and Flowers
+// Contest: Codeforces - Codeforces Round 261 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/459/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <vector>
+#include <numeric>
+#include <stack>
+using namespace std;
+const long long INF = 9223372036854775807;
+const long long MOD = 1e9 + 7;
+ 
+ 
+template<typename T>
+void readvector(vector<T>& v, int n) {
+    v.resize(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+}
+ 
+template <typename T>
+auto SUM(const T& A) {
+    using Elem = typename T::value_type;
+    return std::accumulate(A.begin(), A.end(), Elem(0));
+}
+ 
+#define REP(n) for(int t=0; t<n; t++)
+#define FOR(i, n) for (i = 0; i < n; i++)
+#define FFOR(i, j, n) for(i = j; j < n; j++)
+#define FOR_S(i, n, k) for (i = 0; i < n; i += k)
+#define RFOR(i, n) for (i = n - 1; i >= 0; i--)
+#define RFOR_S(i, n, k) for (i = n - 1; i >= 0; i -= k)
+#define MAX_OF(x, y) ((x > y) ? x : y)
+#define MIN_OF(x, y) ((x > y) ? y : x)
+#define MIN_IN(A) *(std::min_element(A.begin(), A.end()))
+#define MAX_IN(A) *(std::max_element(A.begin(), A.end()))
+#define MAX_AT(A) (std::max_element(A.begin(), A.end()) - A.begin())
+#define MIN_AT(A) (std::min_element(A.begin(), A.end()) - A.begin())
+#define COUNT(V,A) count(V.begin(), V.end(), A);
+#define TESTCASE                                                              \
+int testcase;                                                               \
+std::cin>>testcase;                                                         \
+while(testcase--)
+#define GCD(a,b) std::__gcd(a,b);
+#define LCM(a, b) ((a) / std::__gcd((a), (b)) * (b))
+#define SORT(A) std::sort(A.begin(), A.end());
+#define RSORT(A) std::sort(A.rbegin(), A.rend());
+#define REVERSE(A) std::reverse(A.begin(), A.end());
+#define ANY(container, result, condition)                                      \
+result = false;                                                              \
+for (const auto &e : container)                                              \
+    if (condition(e)) {                                                        \
+    result = true;                                                           \
+    break;                                                                   \
+    }
+#define ALL(container, result, condition)                                      \
+result = true;                                                               \
+for (const auto &e : container)                                              \
+    if (!condition(e)) {                                                       \
+    result = false;                                                          \
+    break;                                                                   \
+    }
+template<typename... Args>
+void print(Args... args) {
+    ((cout << args << " "), ...);
+    cout << "
+";
+}
+ 
+#define ll long long
+#define LL long long
+#define vec vector
+#define pb push_back
+#define popb pop_back
+ 
+LL power(LL x, unsigned long long y) {
+    // returns  x^y % p
+    LL res = 1;
+    x = x % MOD; 
+    while (y > 0) {  
+        if (y & 1) 
+            res = (res*x) % MOD;  
+        y = y>>1;
+        x = (x*x) % MOD;   
+    } 
+    return res; 
+ }
+ 
+ 
+ 
+void speed(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+ll gcd(ll a, ll b){
+	if(b==0)return a;
+	return gcd(b,a%b);
+}
+ 
+int main(){
+    //speed();
+	TESTCASE{
+		int n;cin >> n;
+		vec<ll> v;
+		readvector(v,n);
+		vec<ll> x;
+		for(int i=n-2;i>=0;i--){
+			if(v[i+1]%v[i]!=0){
+				ll y=gcd(max(v[i],v[i+1]),min(v[i],v[i+1]));
+				x.pb(v[i]/y);
+				
+			}
+		}
+		ll lcm=1;
+		if(x.empty())cout << "1
+";
+		else {
+			lcm=x[0];
+			for(int i=1;i<x.size();i++){
+				lcm = (lcm/gcd(max(lcm,x[i]),min(lcm,x[i])))*x[i];
+				
+			}
+			cout << lcm << "
+";
+		}
+		
+		//cout << x << "
+";
+	}
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
