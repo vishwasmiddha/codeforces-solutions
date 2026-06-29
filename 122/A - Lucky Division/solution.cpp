2678f4c4a75aaae8e1083e7c94d0a79e267e3835
Vishwas Middha <dkmidha70@gmail.com>
2026-06-29 07:48:28 +0530

    Add Lucky Division [A] from Codeforces

---
+++ 122/A - Lucky Division/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+ 
+int main(){
+    int n;
+    cin >> n;
+    if(n%4==0 or n%7==0 or n%44==0 or n%77==0 or n%47==0 or n%74==0 or n%444==0 or n%447==0 or n%474==0 or n%477==0 or n%744==0 or n%747==0 or n%774==0 or n%777==0){
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+}
