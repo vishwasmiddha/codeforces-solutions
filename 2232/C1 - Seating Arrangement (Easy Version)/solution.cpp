01a6cdbb301f9e90a68c871f291603e417498015
Vishwas Middha <dkmidha70@gmail.com>
2026-06-29 08:32:32 +0530

    Add Seating Arrangement (Easy Version) [C1] from Codeforces

---
+++ 2232/C1 - Seating Arrangement (Easy Version)/solution.cpp
@@ -0,0 +1,243 @@
+// Problem: B. Bitwise Reversion
+// Contest: Codeforces - Codeforces Round 1057 (Div. 2)
+// URL: https://codeforces.com/contest/2153/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 1500 ms
+ 
+// Problem: B. Pashmak and Flowers
+// Contest: Codeforces - Codeforces Round 261 (Div. 2)
+// URL: https://codeforces.com/problemset/problem/459/B
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+ 
+#include <iostream>    // cin, cout, cerr
+#include <vector>      // vector
+#include <array>       // array
+#include <deque>       // deque
+#include <list>        // list
+#include <forward_list>// forward_list
+#include <stack>       // stack
+#include <queue>       // queue, priority_queue
+#include <set>         // set, multiset
+#include <map>         // map, multimap
+#include <unordered_set> // unordered_set, unordered_multiset
+#include <unordered_map> // unordered_map, unordered_multimap
+#include <string>      // string, stoi, to_string
+#include <cstring>     // memset, strcpy (C-style strings)
+#include <sstream>     // stringstream
+#include <algorithm>   // sort, min, max, reverse, lower_bound, upper_bound
+#include <numeric>     // accumulate, gcd, lcm
+#include <functional>  // greater, less, function
+#include <utility>     // pair, swap, move
+#include <tuple>       // tuple, tie
+#include <cmath>       // sqrt, pow, abs, sin, cos, etc.
+#include <bitset>      // bitset
+ 
+ 
+using namespace std;
+const long long INF = 9223372036854775807;
+const long long MOD = 998244353;
+ 
+ 
+template<typename T>
+void readvector(vector<T>& v, int n) {
+    v.resize(n);
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
+}
+ 
+ 
+ 
+ 
+template <typename T>
+auto SUM(const T& A) {
+    using Elem = typename T::value_type;
+    return std::accumulate(A.begin(), A.end(), Elem(0));
+}
+ 
+void boolprint(bool a){
+    cout << (a?"YES
+":"NO
+");
+}
+ 
+template <typename T>
+void printvec(vector<T> &v){
+    cout << "Vector: {";
+    for(auto c:v)cout << c << ", ";
+    cout << "}
+";
+}
+#define REP(n) for(int t=0; t<n; t++)
+#define FOR(i, n) for (i = 0; i < n; i++)
+#define FFOR(i, j, n) for(i = j; j < n; j++)
+#define FOR_S(i, n, k) for (i = 0; i < n; i += k)
+#define RFOR(i, n) for (i = n - 1; i >= 0; i--)
+#define RFOR_S(i, n, k) for (i = n - 1; i >= 0; i -= k)
+#define MAX_OF(x, y) ((x > y) ? x : y)
+#define MIN_OF(x, y) ((x > y) ? y : x)
+#define MIN_IN(A) *(std::min_element(A.begin(), A.end()))
+#define MAX_IN(A) *(std::max_element(A.begin(), A.end()))
+#define MAX_AT(A) (std::max_element(A.begin(), A.end()) - A.begin())
+#define MIN_AT(A) (std::min_element(A.begin(), A.end()) - A.begin())
+#define COUNT(V,A) count(V.begin(), V.end(), A);
+#define makevec(v,n) \
+vector<int> v;               \
+readvector(v,n);  
+#define TESTCASE                                                              \
+int testcase;                                                               \
+std::cin>>testcase;                                                         \
+while(testcase--)
+#define GCD(a,b) std::__gcd(a,b);
+#define LCM(a, b) ((a) / std::__gcd((a), (b)) * (b))
+#define SORT(A) std::sort(A.begin(), A.end());
+#define RSORT(A) std::sort(A.rbegin(), A.rend());
+#define REVERSE(A) std::reverse(A.begin(), A.end());
+#define ANY(container, result, condition)                                      \
+result = false;                                                              \
+for (const auto &e : container)                                              \
+    if (condition(e)) {                                                        \
+    result = true;                                                           \
+    break;                                                                   \
+    }
+#define ALL(container, result, condition)                                      \
+result = true;                                                               \
+for (const auto &e : container)                                              \
+    if (!condition(e)) {                                                       \
+    result = false;                                                          \
+    break;                                                                   \
+    }
+template<typename... Args>
+void print(Args... args) {
+    ((cout << args << " "), ...);
+    cout << "
+";
+}
+ 
+#define ll long long
+#define LL long long
+#define vec vector
+#define pb push_back
+#define popb pop_back
+ 
+ 
+ 
+ 
+void speed(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+__int128 MOD2=998244353;
+ 
+__int128 gcd(__int128 a, __int128 b){
+    __int128 mini = min(a,b);
+    __int128 maxi=max(a,b);
+    a=maxi;
+    b=mini;
+    if(b==0)return a;
+    __int128 m = (a%b);
+    return gcd(b,m);
+    
+}
+ 
+void print128(__int128 x){
+    if(x==0){ cout << 0; return; }
+ 
+    string s;
+    while(x){
+        s.push_back('0' + x%10);
+        x/=10;
+    }
+    reverse(s.begin(), s.end());
+    cout << s;
+}
+ 
+int main(){
+    speed();
+    TESTCASE{
+        ll n,x,s;cin>>n>>x>>s;
+        string ss;
+        cin>>ss;
+        vector<ll> seats(x,0);
+        ll ambiextro=0;
+        ll sat=0;
+        ll emptytable = x;
+        ll partialseats = 0;
+        //ll fullseats = x*s;
+        for(auto c:ss){
+            if(c=='I'){
+                if(emptytable){
+                    emptytable--;
+                    sat++;
+                    partialseats+=s-1;
+                }
+            } else if (c=='A') {
+                if(partialseats){
+                    partialseats--;
+                    ambiextro++;
+                    sat++;
+                } else if(emptytable) {
+                    emptytable--;
+                    sat++;
+                    partialseats+=s-1;
+                }
+                
+                
+                
+            } else {
+                // extro 
+                if(partialseats){
+                    partialseats--;
+                    sat++;
+                } else if((ambiextro>0) && (emptytable>0)) {
+                    ambiextro--;
+                    sat++;
+                    emptytable--;
+                    partialseats+=s-1;
+                }
+            }
+           
+        }
+        cout << sat<<"
+";
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
